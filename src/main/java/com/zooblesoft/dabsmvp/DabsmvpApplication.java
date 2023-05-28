f5927f646395cfd4dc7ee4783ff78b9a3730791f
Amutheswaran <amutheswaran@outlook.com>
2023-05-28 19:20:08 +0530

    first commit

---
+++ src/main/java/com/zooblesoft/dabsmvp/DabsmvpApplication.java
@@ -0,0 +1,13 @@
+package com.zooblesoft.dabsmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabsmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabsmvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zooblesoft/dabsmvp/DabsmvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooblesoft.dabsmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabsmvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
